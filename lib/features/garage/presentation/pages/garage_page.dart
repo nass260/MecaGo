4d3a15f1117b921153cdec30b4a1c5fd2ac46e3e
nass260 <138371853+nass260@users.noreply.github.com>
2026-09-01 16:17:50 +0200

    Create garage_page.dart

---
+++ lib/features/garage/presentation/pages/garage_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GaragePage extends StatelessWidget {
+  const GaragePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Mon Garage 🚗'),
+      ),
+    );
+  }
+}
