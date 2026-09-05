299bb504e06f25a9e8707c16d4930a161eb3423a
nass260 <138371853+nass260@users.noreply.github.com>
2026-09-05 12:26:49 +0200

    feat: link cloud health validation routine into application bootstrap main lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'core/navigation/app_router.dart';
 import 'core/services/database_helper.dart';
+import 'core/services/database_seeder.dart';
+import 'core/services/cloud_sync_service.dart'; // <-- 1. Importation du service Cloud ajoutée
 
 void main() async {
-  // 1. Verrouille l'initialisation des liaisons Flutter natives (Obligatoire pour l'asynchrone)
+  // Verrouille l'initialisation des liaisons Flutter natives (Obligatoire pour l'asynchrone)
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 2. Initialisation asynchrone sécurisée de notre base de données locale SQLite
+  // Initialisation asynchrone sécurisée de notre base de données locale SQLite
   await DatabaseHelper.instance.initializeDatabase();
 
-  // 3. Lancement officiel de l'écosystème de l'application MecaGo
+  // Déclenchement automatique du peuplement des données de la maquette (Tesla, Clio, 125€)
+  await DatabaseSeeder.seedInitialData();
+
+  // 2. Vérification chirurgicale de la connectivité avec les serveurs Google Firebase
+  const CloudSyncService cloudService = CloudSyncService();
+  await cloudService.checkCloudHealth();
+
+  // Lancement officiel de l'écosystème de l'application MecaGo
   runApp(const MecaGoApp());
 }
 
@@ -18,17 +27,19 @@ class MecaGoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'MecaGo',
-      debugShowCheckedModeBanner: false, // Désactive la bannière "Debug" pour le rendu investisseur
-      
-      // Configuration de notre routeur global connecté configuré au Sprint 2
-      routerConfig: AppRouter.router,
-      
-      // Configuration du thème de base épuré
-      theme: ThemeData(
-        useMaterial3: true,
-        fontFamily: 'SF Pro Display', // Utilisation de la police officielle Apple
+    return ScaffoldMessenger(
+      child: MaterialApp.router(
+        title: 'MecaGo',
+        debugShowCheckedModeBanner: false, // Désactive la bannière "Debug" pour le rendu investisseur
+        
+        // Configuration de notre routeur global connecté configuré au Sprint 2
+        routerConfig: AppRouter.router,
+        
+        // Configuration du thème de base épuré Apple Style
+        theme: ThemeData(
+          useMaterial3: true,
+          fontFamily: 'SF Pro Display',
+        ),
       ),
     );
   }
